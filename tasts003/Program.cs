8f4ada08f03443da41427ff7744d44293398d39a
Anastasia <didyoumissme.19s@gmail.com>
2023-06-18 10:37:45 +0300

    задача 3

---
+++ tasts003/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.Write("N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    array[i] = (int)Math.Pow(i + 1, 3);
+
+}
+System.Console.WriteLine("[" + string.Join(", ", array) + "]");
